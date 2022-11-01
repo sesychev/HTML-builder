1305242b3c404aa94006ab32b09d851b450dbaeb
sesychev <sergeysychou@gmail.com>
2022-11-01 04:48:49 +0300

    feat: done 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,22 +1,22 @@
 const { promises: fs } = require("fs");
 const path = require("path");
 
-const dir = path.join(__dirname, "secret-folder");
+const dirPath = path.join(__dirname, "secret-folder");
 
 (async function (pathFiles) {
   (await fs.readdir(pathFiles, { withFileTypes: true }))
     .filter((dirent) => dirent.isFile())
     .map((dirent) => {
-      const file = path.join(__dirname, "secret-folder", dirent.name);
-      (async function (p) {
-        const stats = await fs.stat(p);
+      const filePath = path.join(__dirname, "secret-folder", dirent.name);
+      (async function (fp) {
+        const stats = await fs.stat(fp);
         let res = stats.size;
         console.log(
           `${dirent.name.substring(0, dirent.name.indexOf("."))} - ${path
-            .extname(file)
+            .extname(filePath)
             .substring(1)} - ${(res / 1024).toFixed(1)}kb`
         );
-      })(file);
+      })(filePath);
     });
   /*
   try {
@@ -37,4 +37,4 @@ const dir = path.join(__dirname, "secret-folder");
     console.error(err);
   }
 */
-})(dir);
+})(dirPath);
